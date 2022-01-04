6b8ebaf87aaa9f19a7e7393366ed7eb0435cae4e
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2022-01-04 21:09:54 +0900

    Spring-Study 스프링 Jdbc Template

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -3,6 +3,7 @@ package hello.hellospring;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -26,7 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new JdbcMemberRepository(dataSource);
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
